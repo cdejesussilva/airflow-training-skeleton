f965f699ecfdeeada503632c715af50f3c273de4
cdejesussilva <45167490+cdejesussilva@users.noreply.github.com>
2018-11-20 13:48:07 +0100

    Corrected typo

--- dags/exercise3.py
+++ dags/exercise3.py
@@ -39,4 +39,4 @@ branching = BranchPythonOperator(task_id="branching",python_callable=_get_weekda
 task_weekday = ["email_Bob","email_Joe","email_Alice","email_Joe","email_Alice","email_Alice","email_Alice"]
 
 for task in task_weekday:
-  print_weekday >> branching >> DummyOperator(task_id=tas_weekday,dag=dag)
+  print_weekday >> branching >> DummyOperator(task_id=task_weekday,dag=dag)
