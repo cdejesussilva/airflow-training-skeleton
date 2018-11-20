4dd408da3244915746e28e28e377c4e87a7f5b7a
cdejesussilva <45167490+cdejesussilva@users.noreply.github.com>
2018-11-20 11:19:22 +0100

    Updated dag.py to test

--- dags/dag.py
+++ dags/dag.py
@@ -1,15 +1,16 @@
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from datetime import date
 
 dag = DAG(
     dag_id="hello_airflow",
     default_args={
         "owner": "godatadriven",
-        "start_date": airflow.utils.dates.days_ago(3),
+        "start_date": date.today(),
     },
 )
 
 BashOperator(
-    task_id="print_exec_date", bash_command="echo {{ execution_date }}", dag=dag
+    task_id="print_exec_date", bash_command="echo 'My new dag!' ", dag=dag
 )
