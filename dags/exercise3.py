9867cce039f082173abc810be4b40feaaf178343
cdejesussilva <45167490+cdejesussilva@users.noreply.github.com>
2018-11-20 13:46:26 +0100

    Imported BranchPythonOperator

--- dags/exercise3.py
+++ dags/exercise3.py
@@ -3,6 +3,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import BranchPythonOperator
 import datetime
 
 args = {"owner": "cdejesussilva", "start_date": airflow.utils.dates.days_ago(14)}
