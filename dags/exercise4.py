7e5a8d33f23efdf50099824a6ee895ff5b01411d
cdejesussilva <45167490+cdejesussilva@users.noreply.github.com>
2018-11-20 15:52:10 +0100

    Create exercise4

---
+++ dags/exercise4.py
@@ -0,0 +1,34 @@
+import airflow
+import airflow.models import DAG
+from airflow.contrib.operators.postgres_to_gcs_operator import (
+    PostgresToGoogleCloudStorageOperator,
+)
+
+
+args = {
+    "owner": "Catia",
+    "start_date": airflow.utils.dates.days_ago(3),
+}
+
+dag = DAG(
+    dag_id="exercise4",
+    default_args[args,
+    schedule_interval="0"
+
+pgsl_to_gcs=
+PostgresToGoogleCloudStorageOperator(
+  task_id = "postgres_to_gcs",
+  sql="SELECT * FROM
+  land_registry_price_paid_uk WHERE
+  transfer_date = '{{ds}}'",
+      bucket="airflow-training-catia",
+      filename="land_registry_price_paid_uk/{{ds}}/properties_{}.json",
+      postgres_conn_id="catia_airflow_training",
+      dag=dag,
+ )
+  
+  
+  
+  
+  
+  
