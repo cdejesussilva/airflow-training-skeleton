38bf69f09a76f424eef228c507dcaf521be9fe39
Catia Silva <cdejesussilva@bol.com>
2018-11-22 10:59:03 +0100

    added bucket

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -18,7 +18,7 @@ dag = DAG(dag_id="exercise4",
 pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     task_id="step1",
     sql="SELECT * FROM land_registry_price_paid_uk WHERE transfer_date = '{{ ds }}'",
-    bucket="exercise4",
+    bucket="airflow-training-catia",
     filename="daily_load_{{ ds }}",
     postgres_conn_id="catia_airflow_training",
     dag=dag,
